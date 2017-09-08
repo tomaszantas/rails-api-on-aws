0e53dccf9c204be96ebe5fd80f3eee0323f533cb
Tom Antas <tom@codepany.com>
2017-09-08 13:13:02 +0200

    Initial commit

--- app/app/controllers/places_controller.rb
+++ app/app/controllers/places_controller.rb
@@ -1,27 +1,16 @@
 class PlacesController < ApplicationController
 
   def index
+
     @places = Place.all
 
-    puts @places.first.coords
-
-    puts RGeo::GeoJSON.encode(@places.first.coords)
-
-    @places.map { |k,v| k.coords = RGeo::GeoJSON.encode(k.coords, json_parser: :json) }
-    #puts
-
-    #parser = RGeo::WKRep::WKTParser.new(nil, :support_ewkt => true)
-
-
-    #render json: RGeo::GeoJSON.decode(@places.to_json, json_parser: :json)
-    #render json: Place.geo_factory.parse_wkt(@places)
     render json: @places.to_json
 
   end
 
   def create
 
-    coords = Place.geo_factory.point(50.324192, 19.037805)
+    coords = Place.geo_factory.point(params.require(:place)[:lat], params.require(:place)[:lng])
 
     @place = Place.new(place_params)
     @place.coords = coords
